b2dec7ddb4be0438ec01f7e7ad5c084c088931bf
Frangel Rodriguez <frangele331@gmail.com>
2023-11-02 12:56:37 -0400

    Update API.js
    Updating port for aws call backend

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 export default {
   add: (param1, param2) => 
-    axios.get(`/api/v1/calculator/addition?param1=${param1}&param2=${param2}`),
+    axios.get(`8090/api/v1/calculator/addition?param1=${param1}&param2=${param2}`),
   subtract: (param1, param2) => 
-    axios.get(`/api/v1/calculator/subtraction?param1=${param1}&param2=${param2}`),
+    axios.get(`8090/api/v1/calculator/subtraction?param1=${param1}&param2=${param2}`),
   multiply: (param1, param2) => 
-    axios.get(`/api/v1/calculator/multiplication?param1=${param1}&param2=${param2}`),
+    axios.get(`8090/api/v1/calculator/multiplication?param1=${param1}&param2=${param2}`),
   divide: (param1, param2) => 
-    axios.get(`/api/v1/nocalculator/division?param1=${param1}&param2=${param2}`)
+    axios.get(`8090/api/v1/nocalculator/division?param1=${param1}&param2=${param2}`)
 };
